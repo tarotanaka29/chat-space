9f0c42cbc4114ae8352bdb4b34288a89230d5b21
田中太朗 <tarotanaka29@gmail.com>
2018-10-21 17:21:28 +0900

    doneメソッド(7-9)
    返ってきたJSONをdoneメソッドで受取り、HTMLを作成　〜　HTMLを追加した分、メッセージ画面を下にスクロール

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,8 +1,32 @@
 $(function(){
-  $('#new_comment').on('submit', function(e){
+  function buildHTML(message){
+    var html = `<div class="chat-main__body">    
+  <div class="chat-main__body--messages-list">
+    <div class="chat-main__message clearfix" data-id="2096">
+      <div class="chat-main__message-name">
+        ${message.user_name}
+      </div>
+      <div class="chat-main__message-time">
+        ${message.created_at}
+      </div>
+      <div class="chat-main__message-body">
+        <p class="chat-main__message__content"> 
+          ${message.content}
+        </p>
+        <div class="chat-main__message__image">
+          ${message.img.url}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+	return html;
+  }
+
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -10,6 +34,17 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      // propでdisabledをfalseに変更してあげる
+      $('#mask__submit').prop("disabled", false);
+      // 送信したら自動的にスクロールしてあげる
+      $('.messages').animate( {scrollTop: $('chat-main__body').offset().top}, 100 );
+      // #new_commentをリセットしてあげる必要がある
+      $('#new_message')[0].reset();
     })
   })
 })
